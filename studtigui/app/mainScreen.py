22f48844fb9d666b79b21cfeed349674262f71cb
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-03-14 20:20:03 +0000

    Add insert into db and initializing fields scripts to Save Form

--- studtigui/app/mainScreen.py
+++ studtigui/app/mainScreen.py
@@ -3,6 +3,12 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import mysql.connector
+
+## DB Connexion string
+conn = mysql.connector.connect(host="127.0.0.1",
+user="root", password="",
+database="bd_student")
 
 
 
@@ -12,13 +18,17 @@ master = Tk()
 #Global Variables
 varFirstName = StringVar()
 varLastName = StringVar()
-varAge = int()
+varAge = IntVar()
 varSexe = StringVar()
 varClasse = StringVar()
 varNationality = StringVar()
 varPhone = StringVar()
 varEmail = StringVar()
 
+
+#################################
+# # # FORMS CREATION SPACCE # # #
+#################################
 
 ## Save student Form: ENREGISTREMENT D'UN ETUDIANT
 def saveFormShow():
@@ -49,13 +59,13 @@ def saveFormShow():
     rbSexeM.grid(row = 4, column = 1, sticky = W, pady = 2)
     rbSexeF = Radiobutton(saveForm, text="F", variable=varSexe, value="Feminin")
     rbSexeF.grid(row = 4, column = 2, sticky = W, pady = 2)
-    cbNationality = ttk.Combobox(saveForm, values=["France","Cote D'Ivoire","Mali","Burkina Faso", "USA", "Canada"], width=37)
+    cbNationality = ttk.Combobox(saveForm, values=["France","Cote D'Ivoire","Mali","Burkina Faso", "USA", "Canada"], width=37, textvariable=varNationality)
     cbNationality.grid(row = 5, column=1, columnspan=2, sticky = W, pady = 2)
-    cbClasse = ttk.Combobox(saveForm, values=["IT RESEAUX","IC RESEAUX","IT GENIE LOGICIEL","IC GENIE LOGICIEL", "IT FINANCE", "IC FINANCE"], width=37)
+    cbClasse = ttk.Combobox(saveForm, values=["IT RESEAUX","IC RESEAUX","IT GENIE LOGICIEL","IC GENIE LOGICIEL", "IT FINANCE", "IC FINANCE"], width=37, textvariable=varClasse)
     cbClasse.grid(row = 6, column=1, columnspan=2, sticky = W, pady = 2)
     
     # Add Buttons Save and erase
-    btAnnuler = ttk.Button(saveForm, text="Annuler", command="")
+    btAnnuler = ttk.Button(saveForm, text="Annuler", command = initSaveStudent)
     btAnnuler.grid(row = 8, column = 1, sticky = W, pady = 2)
     btSave = ttk.Button(saveForm, text="Valider", command = saveStudent )
     btSave.grid(row = 8, column = 2, sticky = W, pady = 2)
@@ -63,13 +73,7 @@ def saveFormShow():
     
     saveForm.title("ENREGISTRER UN ETUDIANT")
     saveForm.grab_set() #Set a form as modal
-
 ## End of SaveForm function
-
-# Save a Student function
-def saveStudent():
-    messagebox.showinfo("Saving","Saved")
-
     
 
 
@@ -90,7 +94,49 @@ def deleteStudentFormShow():
     deleteStudentForm.grab_set()
 
 
+############################################
+# # # DATA PROCESSING FUNCTIONS SPACE # # #
+############################################  
+
+# Save a Student function
+def saveStudent():
+
+    cursor = conn.cursor()
+    reference = {'firstname': varFirstName.get(), 'lastname' : varLastName.get(), 'age' : varAge.get(), 'sexe' : varSexe.get(), 'nationality' : varNationality.get(), 'classe' : varClasse.get()}
+    cursor.execute("""INSERT INTO students (firstname, lastname, age, sexe, nationality, classe) VALUES(%(firstname)s, %(lastname)s, %(age)s, %(sexe)s, %(nationality)s, %(classe)s)""", reference)
+    conn.close()
+
+    # Show Message after saving
+    messagebox.showinfo("Enregistrement","Etudiant: " + varFirstName.get() + " " + varLastName.get() + " enregistré avec succès!" )
+    
+    #Initialise all the fields
+    varFirstName.set("")
+    varLastName.set("")
+    varAge.set(0)
+    varNationality.set("")
+    varClasse.set("")
+# End function Save student
+
+
+# Initialise student saving form
+def initSaveStudent():
+
+    #Initialise all the fields
+    varFirstName.set("")
+    varLastName.set("")
+    varAge.set(0)
+    varNationality.set("")
+    varClasse.set("")
+# End of init 
+
+
+
+
+#############################
+# # # MAIN SCREEN SPACE # # #
+#############################
   
+
 # button widget main menu
 ## Save a student form
 btOpenSaveForm = Button(master, text = "Enregistrer", height=1, width=25, command=saveFormShow)
