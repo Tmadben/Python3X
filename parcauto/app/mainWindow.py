03f63f4b895ef910192814fe40e45b9398667ffe
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-05-30 15:58:10 +0000

    Update of the main screen of clients records

--- parcauto/app/mainWindow.py
+++ parcauto/app/mainWindow.py
@@ -17,6 +17,27 @@ database="parcauto_db")
 master = Tk()
 
 
+#################################
+# # # FORMS CREATION SPACCE # # #
+# # ESPACE POUR LES FENETRES # #
+#################################
+
+def generateTicketFormShow():
+    ticketForm = tk.Toplevel(master)
+
+
+    ticketForm.title("TICKET NUMERIQUE")
+    ticketForm.geometry("280x380")
+    ticketForm.resizable(0, 0) 
+    ticketForm.grab_set()
+
+
+
+
+
+
+
+
 
 #############################
 # # # MAIN SCREEN SPACE # # #
@@ -26,26 +47,32 @@ master = Tk()
 
 # button widget main menu
 ## To generate new ticket
-btOpenSaveForm = Button(master, text = "Générer ticket", height=1, width=25, command='')
+btOpenSaveForm = Button(master, text = "Générer ticket", height=1, width=25, command=generateTicketFormShow)
 btOpenSaveForm.grid(row = 1, column = 0)
+
+## Shut down the app
+btListenregistrement = Button(master, text = "Enregistrements", height=1, width=25, command='')
+btListenregistrement.grid(row = 1, column = 1)
 
 ## To edit the existing tarifs
 btOpenListForm = Button(master, text = "Tarifs associés", height=1, width=25, command='')
-btOpenListForm.grid(row = 1, column = 1)
+btOpenListForm.grid(row = 1, column = 3)
 
 ## Shut down the app
 btQuit = Button(master, text = "Quitter", height=1, width=25, command=master.quit)
-btQuit.grid(row = 1, column = 2)
+btQuit.grid(row = 1, column = 4)
 
 path = "../image/tdkt_logo.png"
 #Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
 img = ImageTk.PhotoImage(Image.open(path))
 
 panel = tk.Label(master, image = img)
-panel.grid(row = 4, column = 0, columnspan=3, sticky=W+E+N+S, padx=2, pady=2)
+panel.grid(row = 4, column = 0, columnspan=5, sticky=W+E+N+S, padx=2, pady=2)
+
+
 
 ##Open The main Screen 
 master.title("GESTION PARKING TDKT Comp-  MENU PRINCIPAL")
-master.geometry("550x400")
+master.geometry("730x400")
 master.resizable(0, 0) 
 mainloop() 
